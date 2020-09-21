c30586701741b0b8f1560630781908245ba14c53
cyan21 <yann.chaysinh@gmail.com>
2020-09-21 13:30:52 +0200

    added new params to step

--- steps/PkgDebian/1.0.0/onExecute.sh
+++ steps/PkgDebian/1.0.0/onExecute.sh
@@ -1,10 +1,28 @@
 
 packageDebian() {
-  local success=true
-  
-  echo "packaging done :D !!!"
+    local success=true
+    local binary=$(find_step_configuration_value "binaryLocalLocation")
 
-  $success
+    jfrog rt ping
+
+    if [ $? -eq 1 ]; then
+        echo "[ERROR][PackageDebian] Artifactory KO"
+        exit 1
+    fi 
+  
+    # priority to  binaryLocalLocation
+    if [ $(ls $binary 2> /dev/null ) -eq 1 ]; then 
+    
+        # couldn't be found --> look into Artifactory
+        # get binary based on build props
+        # build name and number only work when both are specified 
+        local bname=$(find_step_configuration_value "buildName")
+        local bnumber=$(find_step_configuration_value "buildNumber") 
+
+        echo "packaging done :D !!!"
+    fi
+
+    $success
 }
  
 execute_command packageDebian
