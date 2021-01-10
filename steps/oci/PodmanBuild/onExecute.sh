99d3f14b4b669797f687ba5f84f528662438fe5a
cyan21 <yann.chaysinh@gmail.com>
2021-01-10 12:11:34 +0100

    debug 7

--- steps/oci/PodmanBuild/onExecute.sh
+++ steps/oci/PodmanBuild/onExecute.sh
@@ -24,6 +24,10 @@ podmanBuild() {
     echo "Build name: $build_name"
     echo "Push Image: $push_img"
 
+    pwd 
+    
+    ls -la $(pwd)
+    
     # install podman
     if ! which podman ; then 
         sudo echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${operating_system}/ /" |  sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list 
