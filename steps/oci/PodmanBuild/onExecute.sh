59f9c4e46efd54b8134e99e2adca2701b5a9fa94
cyan21 <yann.chaysinh@gmail.com>
2021-01-21 15:40:16 +0100

    fixed typo

--- steps/oci/PodmanBuild/onExecute.sh
+++ steps/oci/PodmanBuild/onExecute.sh
@@ -84,7 +84,7 @@ podmanBuild() {
     cat /etc/containers/registries.conf
     
     # download artifact from Artifactory 
-    if [ -n "$artifact"]; then 
+    if [ -n "$artifact" ]; then 
         echo "[DEBUG] jfrog rt download $artifact ${artifact%%/*.*} --module=myapp --flat=true --build-name=$build_name --build-number=$build_number"
     else
         echo "artifact is NULL"
