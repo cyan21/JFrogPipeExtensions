bcdffdae2503215c85754cc7017cd20d64cb4525
cyan21 <yann.chaysinh@gmail.com>
2021-04-24 16:15:58 +0200

    test fixes

--- steps/cleanup/Artifact/onExecute.sh
+++ steps/cleanup/Artifact/onExecute.sh
@@ -2,7 +2,7 @@
 cleanupArtifact() {
     local success=true
 
-    local cleanup_policy=$(get_resource_name --type "cleanup/Policy" --operation IN)
+    local cleanup_policy=$(get_resource_name --type Policy --operation IN)
 
     local dry_run=$(find_step_configuration_value "dryRun")
 
@@ -12,7 +12,11 @@ cleanupArtifact() {
     
     echo "Cleanup policy name: $cleanup_policy"
 
+    res_timeUnit_workaround=$(find_resource_variable my_cleanup_policy timeUnit)
+    echo "timeUnit: $res_timeUnit_workaround"
+
     res_timeUnit=$(find_resource_variable $cleanup_policy timeUnit)
+    
     echo "timeUnit: $res_timeUnit"
     
     echo "[INFO] Cleanup done"
