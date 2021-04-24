2c3b544fa937c00017e9c22763c7f6f2f636a096
cyan21 <yann.chaysinh@gmail.com>
2021-04-24 16:24:10 +0200

    test access repositories field

--- steps/cleanup/Artifact/onExecute.sh
+++ steps/cleanup/Artifact/onExecute.sh
@@ -3,7 +3,6 @@ cleanupArtifact() {
     local success=true
 
     local cleanup_policy=$(get_resource_name --type cleanup/Policy/latest --operation IN)
-
     local dry_run=$(find_step_configuration_value "dryRun")
 
     echo "[INFO] Starting cleanup ..."
@@ -15,10 +14,12 @@ cleanupArtifact() {
     res_timeUnit_workaround=$(find_resource_variable my_cleanup_policy timeUnit)
     echo "timeUnit: $res_timeUnit_workaround"
 
-    res_timeUnit=$(find_resource_variable $cleanup_policy timeUnit)
+    # res_timeUnit=$(find_resource_variable $cleanup_policy timeUnit)    
+    # echo "timeUnit: $res_timeUnit"
     
-    echo "timeUnit: $res_timeUnit"
-    
+    repos=$(find_resource_variable my_cleanup_policy repositories)
+    echo "repositories: $repos"
+
     echo "[INFO] Cleanup done"
 
     $success
