35122bd02937f271a0fb82589796fff81500ec01
cyan21 <yann.chaysinh@gmail.com>
2021-04-24 15:24:39 +0200

    added dryRun step param

--- steps/cleanup/artifact/onExecute.sh
+++ steps/cleanup/artifact/onExecute.sh
@@ -3,9 +3,12 @@ cleanupArtifact() {
     local success=true
 
     local cleanup_policy=$(get_resource_name --type cleanupPolicy --operation IN)
-    
+    local dry_run=$(find_step_configuration_value "dryRun")
+
     echo "[INFO] Starting cleanup ..."
 
+    echo "DRY RUN MODE: $dry_run"
+    
     echo "Cleanup policy name: $cleanup_policy"
 
     res_timeUnit=$(find_resource_variable $cleanup_policy timeUnit)
