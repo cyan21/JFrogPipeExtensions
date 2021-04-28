89480a2515c840391386174072752717f897b80a
cyan21 <yann.chaysinh@gmail.com>
2021-04-28 21:44:25 +0200

    final version

--- steps/cleanup/Artifact/onExecute.sh
+++ steps/cleanup/Artifact/onExecute.sh
@@ -2,47 +2,33 @@
 cleanupArtifact() {
     local success=true
 
-    local cleanup_policy=$(get_resource_name --type cleanup/Policy/latest --operation IN)
     local dry_run=$(find_step_configuration_value "dryRun")
-
-    echo "[INFO] Starting cleanup ..."
+    local policies=$(find_step_configuration_value "policies")
+    local time_unit=`echo $policies | jq -r ".[].timeUnit"`
+    local time_interval=`echo $policies | jq -r ".[].timeInterval"`
 
     echo "DRY RUN MODE: $dry_run"
-    
-    echo "Cleanup policy name: $cleanup_policy"
+    echo "list of policies: $policies"
+    echo "Time Unit: $time_unit"
+    echo "Time Interval: $time_interval"
 
+    echo "[INFO] Starting cleanup ..."
+    
     # res_timeUnit_workaround=$(find_resource_variable my_cleanup_policy timeUnit)
     # echo "timeUnit: $res_timeUnit_workaround"
 
     # res_timeUnit=$(find_resource_variable $cleanup_policy timeUnit)    
     # echo "timeUnit: $res_timeUnit"
     
-    # repos=$(find_resource_variable my_cleanup_policy repositories)
-    # echo "repositories: $repos"
-    # echo "repository 1: ${repos[0]}"
-    # echo "number of repositories: ${#repos[@]}"
-
-    # # OK if custom resources
-    # configuration:
-    #   cleanupPolicies:
-    #     type: Policy
-    # res_cleanupPolicies=$(find_resource_variable my_cleanup_policies cleanupPolicies)
-    # echo "list of policies: $res_cleanupPolicies"
-
-    local policies=$(find_step_configuration_value "policies")
-    echo "list of policies: $policies"
-
-    local time_unit=`echo $policies | jq -r ".[].timeUnit"`
-    local time_interval=`echo $policies | jq -r ".[].timeInterval"`
-    echo "Time Unit: $time_unit"
-    echo "Time Interval: $time_interval"
+    jfrog plugin install rt-cleanup
 
     for repo in `echo $policies | jq -r ".[].repositories[]"`; do 
-        echo "rt-cleanup clean $repo --time-unit=$time_unit --no-dl=$time_interval"; 
-    done
+        echo "jfrog rt-cleanup clean $repo --time-unit=$time_unit --no-dl=$time_interval"; 
 
-    jfrog plugin install rt-cleanup
-    # jfrog rt-cleanup clean example-repo-local --time-unit=day --no-dl=3
+        if [[ $dry_run == "false" ]]; then
+            jfrog rt-cleanup clean $repo --time-unit=$time_unit --no-dl=$time_interval
+        fi 
+    done
 
     echo "[INFO] Cleanup done"
 
