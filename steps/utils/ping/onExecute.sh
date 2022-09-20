e77aefa232bdd0633635f6eadba10b768c09e27f
cyan21 <yann.chaysinh@gmail.com>
2022-09-20 18:29:30 +0200

    quick debug 2

--- steps/utils/ping/onExecute.sh
+++ steps/utils/ping/onExecute.sh
@@ -1,7 +1,8 @@
 
 pingJPDs() {
     local success=0
-
+    local i=1
+    local ping_ok=0
     local iteration=$(find_step_configuration_value "iteration")
     local sleepBetweenIteration=$(find_step_configuration_value "sleepBetweenIteration")
     local mylist=$(find_step_configuration_value "integrations")
@@ -10,22 +11,24 @@ pingJPDs() {
     echo "wait: $sleepBetweenIteration"
 
     # echo $mylist
-    i=1
+
     for jpd in `echo $mylist | jq -r '.[].name'`; do 
         url="int_${jpd}_url"
         token="int_${jpd}_accessToken"
         # echo ${!url}
         # echo ${!token}
+        echo "start round : i = $i"
 
         echo "[INFO] Configuring CLI ..."
         configure_jfrog_cli --artifactory-url "${!url}/artifactory" --access-token "${!token}" --server-name jpd_$i
-        # jf -v
+        jf c s
         echo "[INFO] Pinging jpd_$i ..."
         cnt=1
-        while [[ $success -eq 0 && $cnt -le $iteration ]]; do  
+        while [[ $ping_ok -eq 0 && $cnt -le $iteration ]]; do  
             jf rt ping --server-id jpd_$i
             if [[ $? -eq 0 ]]; then 
                 echo "[INFO] Ping tentative $cnt / $iteration = OK"
+                ping_ok=1
                 success=1
             else 
                 echo "[INFO] Ping tentative $cnt / $iteration = KO, will retry in  $sleepBetweenIteration second(s)..."
@@ -34,12 +37,13 @@ pingJPDs() {
             fi
         done
         
-        if [[ $success -eq 0 ]]; then 
+        if [[ $ping_ok -eq 0 ]]; then 
+            success=0
             break
-        else
-            let "i+=1"
         fi
-        echo "i : $i"
+        ping_ok=0
+        let "i+=1"
+        echo "end round i : $i"
     done    
 
     echo $success
