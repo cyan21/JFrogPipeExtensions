889a077af1a9d6513cbda6df67f73c653b2a31c6
cyan21 <yann.chaysinh@gmail.com>
2022-09-28 00:08:23 +0200

    [bashV2] modified script

--- steps/utils/bashV2/onStart.sh
+++ steps/utils/bashV2/onStart.sh
@@ -2,21 +2,25 @@
 configureCustomStep() {
     local success=0
     local ping_ok=0
-    local cliPlugin=$(find_step_configuration_value "cliPlugin")
+    local cli_plugin=$(find_step_configuration_value "cliPlugin")
     local intgs=$(find_step_configuration_value "integrations")
     local platform_intg=""
 
-    echo "cliPlugin: $cliPlugin"
+    echo "cliPlugin: $cli_plugin"
     echo "integration: $intgs"
 
     # look for the JFrog Platform Access Token integration
     for intg in `echo $intgs | jq -r '.[].name'`; do 
+        token="int_${platform_intg}_accessToken"
+
         if [[ $token -ne "" ]]; then
             platform_intg=$intg
             break
         fi
     done
 
+    echo  $platform_intg
+    
     if [[ $platform_intg -eq "" ]]; then
         echo "[ERROR] One JFrog Platform Access Token is required for this step."
         exit 1
@@ -24,24 +28,24 @@ configureCustomStep() {
 
     echo "[INFO] Configuring CLI ..."
     local url="int_${platform_intg}_url"
-    local token="int_${platform_intg}_accessToken"
+    local acc_token="int_${platform_intg}_accessToken"
     # echo ${!url}
     # echo ${!token}
-    configure_jfrog_cli --artifactory-url "${!url}/artifactory" --access-token "${!token}" --server-name jpd
+    configure_jfrog_cli --artifactory-url "${!url}/artifactory" --access-token "${!acc_token}" --server-name jpd
     jf c s
     jf rt ping
 
-    jf plugin install $cliPlugin
+    jf plugin install $cli_plugin
 
     if [[ $? -eq 1 ]]; then
-        echo "[ERROR] Could not install or find the $cliPlugin CLI plugin."
+        echo "[ERROR] Could not install or find the $cli_plugin CLI plugin."
         exit 1
     fi 
 
-    jf $cliPlugin -v 
+    jf $cli_plugin -v 
 
     if [[ $? -eq 1 ]]; then
-        echo "[ERROR] Could not execute the $cliPlugin CLI plugin."
+        echo "[ERROR] Could not execute the $cli_plugin CLI plugin."
         exit 1
     fi 
     
